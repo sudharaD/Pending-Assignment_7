460aa5bea8e9c3564738257272e2017de353dc63
sudharaD <sudharad7@gmail.com>
2021-07-12 22:06:08 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css';
+import coverPhoto from './coverPhoto.png';
+import photo from './photo.jpg';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+const App = () => {
+    return (
+        <div>
+
+            <p className="name">game</p>
+        </div>
+    );
+}
+*/
+class App extends React.Component {
+    render() {
+      return (
+        <div>
+            <div className="navBar">
+                <ul>
+                    <div className="rightAlign">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#pages">Pages</a></li>
+                    <li><a href="#blog">Blog</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="contact">Contact Us</a></li>
+                    </div>
+                </ul>
+            </div>
+
+            <div className="check2">
+            <div className="check1">
+            <div className="coverPhoto">
+                <img src={coverPhoto} alt="cover photo" width="100%"></img>
+            </div>
+            <div className="new1"></div>
+            </div>
+            <div className="new2">My Portfolio</div>
+            <div className="new3"><div  class="button"><a>Explore More</a></div></div>
+            </div>
+
+            <div className="reduceOpasity"></div>
+            <div className= "left">
+                <div className="square1">
+                <div className="photo1"><img src={photo} alt="photo"></img></div>
+                </div>
+                
+            </div>
+        </div>
+      );
+    }
+  }
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
